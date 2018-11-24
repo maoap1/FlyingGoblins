28e42480b5df0d9e8667d2fcaaa2ea2b8acfc419
maoap1 <maoap1@gmail.com>
2018-11-24 09:19:52 +0100

    Mockup prefabs

---
+++ FlyingGoblins/Assets/Scripts/TreeObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeObject : MonoBehaviour {
+
+    public float scalingConstant = 1.1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnMouseDown()
+    {
+        transform.localScale += new Vector3(transform.localScale.x * scalingConstant,
+                                            transform.localScale.y * scalingConstant,
+                                            0);
+    }
+}
