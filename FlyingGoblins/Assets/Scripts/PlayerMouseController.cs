508d562e725ae7fa9f74e9921fb2b6d3f7b90047
maoap1 <maoap1@gmail.com>
2018-11-24 13:48:51 +0100

    mouse

---
+++ FlyingGoblins/Assets/Scripts/PlayerMouseController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 targetPoint = hit.point;
+                targetPoint.z = 0;
+                targetPoint.Normalize();
+                transform.Translate(targetPoint);
+            }
+
+
+        }
+    }
+}
