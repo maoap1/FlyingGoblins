3a9abf109cf4376ed2c3d933f83c79d5ca1c4fa8
Tomáš Jakubec <samot.cebukaj@seznam.cz>
2018-11-24 16:28:23 +0100

    slidery

---
+++ FlyingGoblins/Assets/Scripts/StatsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsController : MonoBehaviour {
+
+    public PlayerStats playerStats;
+    public Slider waterslider;
+    public Slider foodslider;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        waterslider.value = playerStats.water;
+        foodslider.value = playerStats.food;
+	}
+}
---
+++ FlyingGoblins/Assets/Scripts/TextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+    public PlayerStats playerStats;
+    Text text;
+    public float lack = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+        text = this.GetComponent<Text>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "Warning!";
+        text.enabled = false;
+        if(playerStats.water < lack)
+        {
+            text.text += "\n You are thirsty!";
+            text.enabled = true;
+        }
+	}
+}
