d093281f2c3dcb041884421f96c31370e8291945
maoap1 <maoap1@gmail.com>
2018-11-24 09:55:17 +0100

    MouseOver script

---
+++ FlyingGoblins/Assets/Scripts/MouseOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour {
+
+	public float scalingConstant = 1.1f;
+
+    private Vector3 oldScale;
+    private Vector3 newScale;
+
+    // Use this for initialization
+    void Start()
+    {
+        oldScale = transform.localScale;
+        newScale = new Vector3(oldScale.x * scalingConstant, oldScale.y * scalingConstant, oldScale.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        transform.localScale = newScale;
+    }
+    void OnMouseExit()
+    {
+        transform.localScale = oldScale;
+    }
+}
