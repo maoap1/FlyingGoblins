0eb8de5b4b20ae297d53965180d21e6c5e020e35
Tomáš Jakubec <samot.cebukaj@seznam.cz>
2018-11-24 22:02:42 +0100

    death

---
+++ FlyingGoblins/Assets/Scripts/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Singleton : MonoBehaviour
+{
+    public int typeOfDeath;
+    private static Singleton instance = null;
+    public static Singleton Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+   
+}
