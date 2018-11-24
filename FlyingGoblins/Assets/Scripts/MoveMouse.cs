dcd835f050285e90a6d21c5d4ce6b6bcf681a418
Tomáš Jakubec <samot.cebukaj@seznam.cz>
2018-11-24 13:22:21 +0100

    pohyb šipkami

--- FlyingGoblins/Assets/Scripts/MoveMouse.cs
+++ FlyingGoblins/Assets/Scripts/MoveMouse.cs
@@ -1,23 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class Nav : MonoBehaviour {
-    
-    public NavMeshAgent nav;
+public class MoveMouse : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-       
+		
 	}
-
-    void OnMouseDown()
-    {
-        nav.SetDestination(this.transform.position);
-    }
 }
