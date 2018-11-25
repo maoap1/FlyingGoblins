5a4ba0ff054f7f7a49b1ca7794ab651c450ebb75
Tomáš Jakubec <samot.cebukaj@seznam.cz>
2018-11-25 15:56:02 +0100

    main

---
+++ FlyingGoblins/Assets/Scripts/GUI/DeathButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BackToMenuButtonDown()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestratGameButtonDown()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitButtonDown()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
