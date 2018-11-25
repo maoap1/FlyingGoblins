6d6e229ce9b032c8f2602ad3b9c2d9c14d2f5e34
maoap1 <maoap1@gmail.com>
2018-11-25 15:30:41 +0100

    structural changes audio

---
+++ FlyingGoblins/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioScript : MonoBehaviour
+{
+    
+	void Awake()
+    {
+        AudioSource ambientSound = null;
+        AudioSource menuSound = null;
+
+        foreach (Transform child in Singleton.Instance.gameObject.transform)
+         {
+            if (child.tag == "ambientSound")
+            {
+                ambientSound = child.gameObject.GetComponent<AudioSource>();
+            }
+            if (child.tag == "menuSound")
+            {
+                menuSound = child.gameObject.GetComponent<AudioSource>();
+            }
+         }
+
+
+        ambientSound.mute = true;
+        menuSound.mute = true;
+        string activeScene = SceneManager.GetActiveScene().name;
+        if ((activeScene == "MainMenu")||(activeScene == "Story"))
+        {
+            menuSound.enabled = true;
+            menuSound.mute = false;
+            ambientSound.enabled = false;
+        }
+        else
+        {
+            menuSound.enabled = false;
+            ambientSound.enabled = true;
+            ambientSound.mute = false;
+        }
+    }
+}
