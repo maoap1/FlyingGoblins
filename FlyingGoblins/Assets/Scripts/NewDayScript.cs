faef6bfc54b1136746e0dcd31299776e94f96035
Tomáš Jakubec <samot.cebukaj@seznam.cz>
2018-11-25 19:48:36 +0100

    final

--- FlyingGoblins/Assets/Scripts/NewDayScript.cs
+++ FlyingGoblins/Assets/Scripts/NewDayScript.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Text;
 
 public class NewDayScript : MonoBehaviour {
 
     public TextMeshProUGUI dayCount;
-    public TextMeshProUGUI newObjectives;
     public TextMeshProUGUI infoObjectives;
     public int dayNumber;
+    public MainObjective mainObjective;
 
     void OnEnable()
     {
         dayNumber ++;
         dayCount.SetText("Day " + dayNumber.ToString());
         Singleton.Instance.days = dayNumber;
-        infoObjectives.SetText(newObjectives.text);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Objective :");
+            sb.AppendLine(mainObjective.currentObjective.description);
+            infoObjectives.SetText(sb);
+        
     }
 }
