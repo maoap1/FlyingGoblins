8ad1d1937e3f3427f358b463d16b24a85d5a1879
Tomáš Jakubec <samot.cebukaj@seznam.cz>
2018-11-25 12:35:04 +0100

    player

--- FlyingGoblins/Assets/Scripts/NewDayScript.cs
+++ FlyingGoblins/Assets/Scripts/NewDayScript.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NewDayScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public TextMeshProUGUI dayCount;
+    public int dayNumber;
+    Singleton singleton;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        singleton = Singleton.Instance;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+      
+    }
+
+    void OnEnable()
+    {
+        dayNumber ++;
+        dayCount.SetText("Day " + dayNumber.ToString());
+        singleton.days = dayNumber;
+    }
 }
